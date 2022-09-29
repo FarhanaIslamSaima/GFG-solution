744df075c74575a5b62e31df1cb1ee57e1d61abe
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-09-29 12:47:53 +0600

    updated

---
+++ Insertionsort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include<string.h>
+#include <limits.h>
+#include <vector>
+#include <bits/stdc++.h>
+#include<array> 
+#include <unordered_map>
+#include<cmath>
+
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int arr[a];
+    for(int i=0;i<a;i++){
+        cin>>arr[i];
+    }
+
+
+    for(int i=0;i<a;i++){
+        int temp=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>temp){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=temp;
+    }
+
+    for(int i=0;i<a;i++){
+        cout<<arr[i]<<" ";
+    }
+
+
+
+
+    return 0;
+}
