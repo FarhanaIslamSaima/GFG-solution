b48a78c55e554945dd18bee14c4791fde52258f2
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-10-14 23:03:30 +0600

    updated

---
+++ string/lowertoupper.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include<string.h>
+#include <limits.h>
+#include <vector>
+#include <bits/stdc++.h>
+#include<array> 
+#include <unordered_map>
+#include<cmath>
+
+
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+   for (int i=0;i<s.length();i++){
+    cout<<(char)toupper(s[i]);
+   }
+  
+    return 0;
+}
---
+++ string/sortstr2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include<string.h>
+#include <limits.h>
+#include <vector>
+#include <bits/stdc++.h>
+#include<array> 
+#include <unordered_map>
+#include<cmath>
+
+
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s;
+    return 0;
+}
