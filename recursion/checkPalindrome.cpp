0d4cc5015d38d8c3a1ee35132d4fe2d299c91570
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-10-22 12:37:26 +0600

    updated

---
+++ recursion/checkPalindrome.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool checkPalindrome(string s,int i){
+    string res="";
+    int len=s.length();
+
+    if(s[i]!=s[len-i-1]){
+      return false;
+    }
+ 
+    checkPalindrome(s,i+1);
+    return true;
+
+ 
+
+ 
+
+
+
+
+}
+int main(){
+    string s;
+    cin>>s;
+    if(checkPalindrome(s,0)){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    return 0;
+}
