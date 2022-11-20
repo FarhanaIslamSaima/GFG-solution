da5fdd681b8f48bfdccf79d6f0738d32997c0a2c
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-11-20 09:51:53 +0600

    updated

---
+++ DP/string/longestCommonsubstr.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+string f(int ind1,int ind2,string &s1, string &s2, vector<vector<int>>&dp){
+    string temp="";
+ 
+    for(int i=0;i<=ind1;i++){
+        dp[i][0]=0;
+    }
+     for(int i=0;i<=ind2;i++){
+        dp[0][i]=0;
+    }
+    for(int i=1;i<=ind1;i++){
+        for(int j=1;j<=ind2;j++){
+            if(s1[i-1]==s2[j-1]){
+         dp[i][j]=1+dp[i-1][j-1];
+
+    }
+    else{
+        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+    }
+ 
+
+
+        }
+    }
+  
+  int i=ind1;
+  int j=ind2;
+  int len=dp[ind1][ind2];
+int index=len-1;
+for(int i=0;i<len;i++){
+    temp+='$';
+}
+
+  while(i>0 && j>0){
+    if(s1[i-1]==s2[j-1]){
+        
+        temp[index]=s1[i-1];
+        index--;
+        i--;
+        j--;
+
+    }
+
+else if(dp[i-1][j]>dp[i][j-1]){
+    i--;
+}
+else{
+    j--;
+
+}
+  }
+
+return temp;
+
+   
+    
+}
+
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    int ind1=s1.length();
+    int ind2=s2.length();
+    vector<vector<int>>dp(ind1+1,vector<int>(ind2+1,0));
+    cout<<f(ind1,ind2,s1,s2,dp);
+
+
+    
+    return 0;
+}
