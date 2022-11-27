05ae9ef6fe84a19a785d7fa82b712888a585a8d3
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-11-27 19:46:53 +0600

    updated

--- DP/stocks/tabBuySellStock4.cpp
+++ DP/stocks/tabBuySellStock4.cpp
@@ -3,27 +3,26 @@
 #include<algorithm>
 using namespace std;
 
-int f(int a, int k,int arr[],vector<vector<vector<int>>>&dp){
+int f(int a,int tranNo, int k,int arr[], vector<vector<int>>&dp){
  
-   
 
-    for(int i=a-1;i>=0;i--){
-        for(int j=0;j<=1;j++){
-            for(int p=1;p<=k;p++){
-                if(j==1){
-                    dp[i][j][p]=max(-arr[i]+dp[i+1][0][p],0+dp[i+1][1][p]);
 
-                }
-                else{
-                    dp[i][j][p]=max(arr[i]+dp[i+1][1][p-1],0+dp[i+1][0][p]);
-                }
-            }
-        }
+int profit;
+
+for(int i=a-1;i>=0;i--){
+    for(int j=tranNo-1;j>=0;j--){
+       if(j%2==0){
+        dp[i][j]=max(-arr[i]+dp[i+1][j+1],0+dp[i+1][j]);
+       }
+       else{
+        dp[i][j]=max(arr[i]+dp[i+1][j+1],0+dp[i+1][j]);
+       }
     }
+}
+
+return dp[0][0];
 
 
-
-  return dp[0][1][k];
 
 
 
@@ -36,7 +35,8 @@ int main(){
     for(int i=0;i<a;i++){
         cin>>arr[i];
     }
-    vector<vector<vector<int>>>dp(a+1,vector<vector<int>>(2,vector<int>(k+1,0)));
-    cout<<f(a,k,arr,dp);
+    int tranNo=2*k;
+   vector<vector<int>>dp(a+1,vector<int>(tranNo+1,0));
+    cout<<f(a,tranNo,k,arr,dp);
     return 0;
 }
