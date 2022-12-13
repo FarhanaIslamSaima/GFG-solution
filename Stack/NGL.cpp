69cb10052141ecbbc2db4b3249b766cbeaad20d5
FarhanaIslamSaima <farhanaislamsaim@gmail.com>
2022-12-13 19:56:49 +0600

    updated

---
+++ Stack/NGL.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main(){
+    
+int a;
+cin>>a;
+int arr[a];
+for(int i=0;i<a;i++){
+    cin>>arr[i];
+}
+vector<int>v;
+stack<int>s;
+for(int i=0;i<a;i++){
+
+    if(s.size()==0){
+        v.push_back(-1);
+    }
+    else if(s.size()>0 && s.top()>arr[i]){
+        v.push_back(s.top());
+    }
+    else if(s.size()>0 && s.top()<=arr[i]){
+        while(s.size()>0 && s.top()<=arr[i]){
+            s.pop();
+        }
+        if(s.size()==0){
+            v.push_back(-1);
+        }
+        else{
+            v.push_back(s.top());
+        }
+    }
+           s.push(arr[i]);
+ 
+
+}
+
+for(int i=0;i<v.size();i++){
+    cout<<v[i]<<" ";
+}
+    return 0;
+}
